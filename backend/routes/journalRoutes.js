e0ced93e38a8bcae0df3ba8e0f38b300cc3c172a
DODO RJ <dodo42dtp@gmail.com>
2025-08-21 14:56:07 +0100

    rousoudre le backend  et nmp start:all

--- backend/routes/journalRoutes.js
+++ backend/routes/journalRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const journalController = require('../controllers/journalController');
+const journalController = require('../controllers/journal.controller');
 
 router.post('/', journalController.createJournal);
 router.get('/:userId', journalController.getUserJournals);
