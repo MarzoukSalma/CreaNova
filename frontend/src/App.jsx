39a4a41a9c4cb2570569ab551e6c21eaeba74ef0
DODO RJ <dodo42dtp@gmail.com>
2025-08-22 13:36:31 +0100

    "block non auth"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,63 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar'; // ✅ ajoute cet import
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import JournalCreativite from './pages/journal.jsx';
 import StudioRevesCreatifs from './pages/dream.jsx';
-import Galerie from './pages/inspiration.jsx'; // Assurez-vous que ce composant existe
-import Chatboot from './pages/chatboot.jsx'; // Assurez-vous que ce composant existe
-import Sittings from './pages/sittings.jsx'; // Assurez-vous que ce composant existe
+import Galerie from './pages/inspiration.jsx';
+import Chatboot from './pages/chatboot.jsx';
+import Sittings from './pages/sittings.jsx';
 import Login from './pages/login.jsx';
+import ProtectedRoute from './components/ProtectedRoute'; // ✅ import correctement en haut
+
 const App = () => {
   return (
     <Router>
       <Navbar /> {/* Navbar visible sur toutes les pages */}
+      
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/galerie" element={<Galerie />} />
-        <Route path="/muse" element={<Chatboot />} />
-        <Route path="/sittings" element={<Sittings />} />
-        <Route path="/reves" element={<StudioRevesCreatifs />} />
-        <Route path="/journal" element={<JournalCreativite />} />
+
+        {/* toutes les pages protégées */}
+        <Route
+          path="/galerie"
+          element={
+            <ProtectedRoute>
+              <Galerie />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/muse"
+          element={
+            <ProtectedRoute>
+              <Chatboot />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/sittings"
+          element={
+            <ProtectedRoute>
+              <Sittings />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/reves"
+          element={
+            <ProtectedRoute>
+              <StudioRevesCreatifs />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/journal"
+          element={
+            <ProtectedRoute>
+              <JournalCreativite />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
