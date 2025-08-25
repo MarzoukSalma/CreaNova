583e8f613e18ab6b6c0117f547406f5022b98cde
MarzoukSalma <salmamarzouk07@gmail.com>
2025-08-25 19:56:05 +0100

    ajout de workspace

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar'; // ✅ ajoute cet import
-import JournalCreativite from './pages/journal.jsx';
-import StudioRevesCreatifs from './pages/dream.jsx';
-import Galerie from './pages/inspiration.jsx'; // Assurez-vous que ce composant existe
-import Chatboot from './pages/chatboot.jsx'; // Assurez-vous que ce composant existe
-import Sittings from './pages/sittings.jsx'; // Assurez-vous que ce composant existe
-import Login from './pages/login.jsx';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/Navbar"; // ✅ ajoute cet import
+import JournalCreativite from "./pages/journal.jsx";
+import StudioRevesCreatifs from "./pages/dream.jsx";
+import Galerie from "./pages/inspiration.jsx"; // Assurez-vous que ce composant existe
+import Chatboot from "./pages/chatboot.jsx"; // Assurez-vous que ce composant existe
+import Sittings from "./pages/sittings.jsx"; // Assurez-vous que ce composant existe
+import Login from "./pages/login.jsx";
+import WorkspacePage from "./pages/workspace.jsx";
 const App = () => {
   return (
     <Router>
@@ -18,6 +24,7 @@ const App = () => {
         <Route path="/sittings" element={<Sittings />} />
         <Route path="/reves" element={<StudioRevesCreatifs />} />
         <Route path="/journal" element={<JournalCreativite />} />
+        <Route path="/workspace" element={<WorkspacePage />} />
       </Routes>
     </Router>
   );
